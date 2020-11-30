b5227f297317b3e6a42534d836d8999196eac3b9
Jesse Okoro <27390903+jesmanto@users.noreply.github.com>
2020-11-30 12:41:22 +0100

    Create Main.java
    Added Main.java file

---
+++ Main.java
@@ -0,0 +1,30 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    int num = 4;
+    char alph = 'A';
+    String name = "Jesse";
+    boolean food = true;
+    float dec = 23.233f;
+    double precision = 123.234535;
+    Jesse.haveEaten("null");
+  }
+}
+class Jesse{
+  public boolean eaten(){
+    String myArray[] = {};
+    Main.main(myArray);
+    return true;
+  }
+  // Create a static function called haveEaten that returns a boolean
+  // The function takes a string argument, food
+  // The function returns false if the input is empty or null, and returns true if otherwise.
+  
+  public static boolean haveEaten(String food){
+    if(food == "" || food == null){
+      return false;
+    }
+    System.out.print("Yes, I have eaten " + food);
+    return true;
+  }
+}
